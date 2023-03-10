b2e3768f103a83884a0ef10590fce86112541686
Marcus Bernroth <marcus.bernroth@oimsweden.com>
2023-03-10 15:37:33 +0100

    baby steps

--- src/three.js
+++ src/three.js
@@ -7,26 +7,27 @@ export default function begin(floatArray) {
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+    1,
+    10000
   );
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const geometry = new THREE.BoxGeometry(2, 1, 1);
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
 
-  camera.position.z = 5;
+  camera.position.z = 55;
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
   function animate() {
-    // cube.position.x = floatArray[0] / 1000;
-    // cube.position.y = floatArray[1] / 1000;
-    // cube.position.z = floatArray[2] / 1000;
-    // cube.rotation.x = floatArray[0] / 1000;
-    // cube.rotation.y = floatArray[1] / 1000;
-    cube.rotation.z = floatArray[2] / 1000;
+    cube.position.x += floatArray[3] / 1000 || 0;
+    cube.position.y += floatArray[4] / 1000 || 0;
+    // cube.position.z += floatArray[2] / 1000 || 0;
+
+    // cube.rotation.x += floatArray[0] / 1000 || 0;
+    // cube.rotation.y += floatArray[1] / 1000 || 0;
+    // cube.rotation.z += floatArray[2] / 1000 || 0;
 
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
